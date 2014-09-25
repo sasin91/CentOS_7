00eb8ceb1eb89ace26bcadaa19e8e5ed0970821e
Jonas Kervin Hansen <lotd@localhost.localdomain>
2014-09-25 21:51:48 +0200

    CentOS 7 Ace + Devel script. Committer: sasin91 <lotd@fedora_20>

---
+++ Ace_Centos.sh
@@ -0,0 +1,4 @@
+ACE_VERSION=6.2.8-48.1; sudo yum install \
+http://download.opensuse.org/repositories/devel:/libraries:/ACE:/micro/CentOS_7/$(arch)/ace-$ACE_VERSION.$(arch).rpm \
+http://download.opensuse.org/repositories/devel:/libraries:/ACE:/micro/CentOS_7/$(arch)/ace-devel-$ACE_VERSION.$(arch).rpm \
+http://download.opensuse.org/repositories/devel:/libraries:/ACE:/micro/CentOS_7/$(arch)/mpc-$ACE_VERSION.$(arch).rpm
